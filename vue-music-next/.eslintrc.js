512986427820207510f9f652691c3a2a7b9c3609
yanhhhhhh <1801084586@qq.com>
2021-05-31 00:32:13 +0800

    header、tab

--- vue-music-next/.eslintrc.js
+++ vue-music-next/.eslintrc.js
@@ -5,13 +5,15 @@ module.exports = {
   },
   extends: [
     'plugin:vue/vue3-essential',
-    '@vue/standard'
+    //'@vue/standard'
   ],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'indent': 0,
+    'space-before-function-paren': 0
   }
 }
